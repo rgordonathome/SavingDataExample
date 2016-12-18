29ab863c409d50af59d2b4e18ec536b82a7b71b7
Russell Gordon <rgordon@rsgc.on.ca>
2016-12-18 15:41:03 -0500

    Created an outlet collection for student first names.

--- SavingDataExample/ViewController.swift
+++ SavingDataExample/ViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstNames: [UITextField]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print(firstNames.count)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
